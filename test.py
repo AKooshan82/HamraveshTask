2f15b093c19d35d27573b1a2f8551a76a5cebe82
Kooshan Fattah <kooshan@MacBook-Pro-3.local>
2026-07-09 12:57:40 +0330

    The lines get parsed correctly

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ else:
 num_lines = sum(1 for _ in open(file_path))
 print(num_lines)
 
-pattern = r"(?P<ip_address>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?:\D{1,5})(?P<date>\[\d{2}\/\D{3}\/\d{4}\:\d{2}\:\d{2}\:\d{2} \+0000\])\D(?P<message>\"\w+ \/ \w+\/[^\"]+\")\D(?P<error_code>\d{1,3})\D(?P<bite_size>\d{1,10})\D{5}(?P<user_agent>\"[^\"]+\")"
+pattern = r"(?P<ip_address>\d{1,3}.\d{1,3}\.\d{1,3}\.\d{1,3})(?:\D{1,5})(?P<date>\[\d{2}\/\D{3}\/\d{4}\:\d{2}\:\d{2}\:\d{2} \+0000\])\D(?P<message>\"\w+[^\"]+\")\D(?P<error_code>\d{1,3})\D(?P<bite_size>\d{1,10})\D{5}(?P<user_agent>\"[^\"]+\")"
 with open(file_path, "r") as file:
     for line in file:
         input()
