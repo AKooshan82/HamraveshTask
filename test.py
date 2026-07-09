cd7e1713c751bbfb39129cb8320d11bbc212b05e
Kooshan Fattah <kooshan@MacBook-Pro-3.local>
2026-07-09 13:15:55 +0330

    Added error rate, corrupt line detection, unique ip count

--- test.py
+++ test.py
@@ -21,9 +21,24 @@ num_lines = sum(1 for _ in open(file_path))
 print(num_lines)
 
 pattern = r"(?P<ip_address>\d{1,3}.\d{1,3}\.\d{1,3}\.\d{1,3})(?:\D{1,5})(?P<date>\[\d{2}\/\D{3}\/\d{4}\:\d{2}\:\d{2}\:\d{2} \+0000\])\D(?P<message>\"\w+[^\"]+\")\D(?P<error_code>\d{1,3})\D(?P<bite_size>\d{1,10})\D{5}(?P<user_agent>\"[^\"]+\")"
+
+unique_ips = set()
+errors= 0
+
+corrupt_lines = 0
 with open(file_path, "r") as file:
     for line in file:
-        input()
         match = re.search(pattern,line)
         if match is not None:
-            print(match.group("ip_address"))
+            unique_ips.add(match.group("ip_address"))
+            if bool(re.search("^[4-5]",match.group("error_code"))) :
+                errors+=1
+        else:
+            corrupt_lines+=1
+
+true_error_rate = (errors/ (num_lines - corrupt_lines ) )*100
+print(f"Number of Corrupted Lines : {corrupt_lines}")
+print(f"Number of Lines(Requests) :{num_lines}")
+print(f"Number of Unique Ip addresses:{len(unique_ips)}")
+print("Error rate is (number of 5xx and 4xx requests):%.2f"%true_error_rate)
+
