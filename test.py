86eb90092b288c1754e52cf3d6467aff17dcf588
Kooshan Fattah <kooshan@MacBook-Pro-3.local>
2026-07-09 14:03:28 +0330

    added the time table for traffic

--- test.py
+++ test.py
@@ -24,13 +24,15 @@ else:
 num_lines = sum(1 for _ in open(file_path))
 print(num_lines)
 
-pattern = r"(?P<ip_address>\d{1,3}.\d{1,3}\.\d{1,3}\.\d{1,3})(?:\D{1,5})(?P<date>\[\d{2}\/\D{3}\/\d{4}\:\d{2}\:\d{2}\:\d{2} \+0000\])\D(?P<message>\"\w+[^\"]+\")\D(?P<error_code>\d{1,3})\D(?P<byte_size>\d{1,10})\D{5}(?P<user_agent>\"[^\"]+\")"
+pattern = r"(?P<ip_address>\d{1,3}.\d{1,3}\.\d{1,3}\.\d{1,3})(?:\D{1,5})(?P<date>\[\d{2}\/\D{3}\/\d{4}\:(?P<hour>\d{2})\:\d{2}\:\d{2} \+0000\])\D(?P<message>\"\w+[^\"]+\")\D(?P<error_code>\d{1,3})\D(?P<byte_size>\d{1,10})\D{5}(?P<user_agent>\"[^\"]+\")"
 
 unique_ips = set()
 traffic = {}
+hour_traffic = {}
 errors= 0
 lines=0
 corrupt_lines = 0
+
 with open(file_path, "r") as file:
     for line in file:
         match = re.search(pattern,line)
@@ -45,6 +47,11 @@ with open(file_path, "r") as file:
                     "malicious_attemp" : 0
                 }
             traffic[match.group("ip_address")]["bytes"]+=bytes_sent
+            #each hour traffic
+            if match.group("hour") not in hour_traffic:
+                hour_traffic[match.group("hour")] =bytes_sent
+            else:
+                hour_traffic[match.group("hour")]+=bytes_sent
             #error rate
             if bool(re.search("^5",match.group("error_code"))) :
                 errors+=1
@@ -72,9 +79,14 @@ print("Top 10 Ips with the most 4xx errors :")
 for ip, info in top_10_malicious:
     print(ip, info["malicious_attemp"])
 
+
+print(f"{'Hour of the day':<18} {'Traffic':>10}")
+print("-" * 30)
+for hour,traffic in hour_traffic.items():
+    print(f"{hour:<18} {traffic:>10}")
+
 end_time = time.perf_counter()
 execution_time = end_time - start_time
 print("==================================")
-
 print("Program took %.2f seconds" %execution_time)
 
